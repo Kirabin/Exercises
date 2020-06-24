12af606d3e3b2564ed26035a066e64d27f2fef26
Kirabin <ryabink.2000@icloud.com>
2020-06-24 14:18:52 +0300

    bit_counting.py

---
+++ codewars/python/kyu6/bit_counting.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
+
+def countBits(n):
+
+	return bin(n)[2:].count('1')
+
+
+
+assert countBits(0) == 0
+assert countBits(4) == 1
+assert countBits(7) == 3
+assert countBits(9) == 2
+assert countBits(10) == 2
