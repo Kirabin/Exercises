7680de665e2c243751b7be6b5ce6d16a9618e50b
kirabin <kirabin.one@gmail.com>
2021-07-09 14:13:15 +0300

    first swift upload

---
+++ swift/kuy5/gapInPrimes.swift
@@ -0,0 +1,34 @@
+extension Int {
+    func isSimple() -> Bool {
+        var i = 2
+        while i*i <= self {
+            if self % i == 0 {
+                return false
+            }
+            i += 1
+        }
+        return true
+    }
+}
+
+func gap(_ g: Int, _ m: Int, _ n: Int) -> (Int, Int)? {
+    var previousSimple:Int? = nil
+
+    for i in m...n {
+        if i.isSimple() {
+            if let prev = previousSimple {
+                if i - prev == g {
+                    return (prev, i)
+                }
+            }
+            previousSimple = i
+        }
+    }
+    return nil
+}
+
+assert(gap(2, 100, 110)! == (101, 103))
+assert(gap(4, 100, 110)! == (103, 107))
+assert(gap(6, 100, 110) == nil)
+assert(gap(2,10000000,11000000)! == (10000139, 10000141))
+
